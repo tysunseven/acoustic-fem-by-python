996b0259b4c08d9503678a619e746f5033c9394a
孙天阳 <tysun@mail.ustc.edu.cn>
2026-01-13 19:55:56 +0800

    Update __init__.py

--- acoustic_fem/__init__.py
+++ acoustic_fem/__init__.py
@@ -4,4 +4,6 @@ from .simulation import AcousticSimulator
 # 也可以直接暴露常用的配置，方便外部修改
 from .defaults import PHYSICS_PARAMS, GRID_CONFIG
 
-__all__ = ['AcousticSimulator', 'PHYSICS_PARAMS', 'GRID_CONFIG']
+from .utils import load_structures_from_mat, save_results_to_npy
+# 更新 __all__
+__all__ = ['AcousticSimulator', 'PHYSICS_PARAMS', 'GRID_CONFIG', 'load_structures_from_mat', 'save_results_to_npy']
